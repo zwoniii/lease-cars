3cdfce70f08816b02f2884eb6277941235e317f0
DESKTOP-T2D8UUS\ira <ira.zvonova@outlook.com>
2023-04-15 12:20:18 +0300

    main page adaptive

--- js/main.js
+++ js/main.js
@@ -22,6 +22,13 @@ function open(evt) {
   document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
 }
 
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu__list--active')
+})
+
 
 const swiper = new Swiper(".swiper", {
   effect: "fade",
